d7a80a35e1da8e58db39cf5c77cb8d8a0150581f
TanyasiPills <matyasg760@gmail.com>
2026-02-26 15:39:10 +0100

    progress

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ use std::cmp::{Ordering};
 use iced::{Background, Color, Element, Length, alignment, Task, Event, Subscription, event, window};
 use iced::widget::{column, container, Text};
 
-use libsais::{BwtConstruction, ThreadCount};
+use libsais::bwt::Bwt;
+use libsais::{BwtConstruction, ThreadCount, UnBwt};
 
 use rayon::prelude::*;
 
@@ -61,17 +62,33 @@ impl Ord for HuffData {
     }
 }
 
-fn bwt_block(data: &[u8]) -> (Vec<u8>, usize) {
+fn bwt_encode(data: &[u8]) -> (Vec<u8>, usize) {
     let res = BwtConstruction::for_text(data)
         .with_owned_temporary_array_buffer32()
         .multi_threaded(ThreadCount::openmp_default())                  
         .run()
         .expect("BWT failed");
 
-    let bwt: Vec<u8> = res.bwt().to_vec();
-    let orig_idx = res.primary_index();
+    let test = res.into_parts();
 
-    (bwt, orig_idx)
+    (test.0, test.1)
+}
+
+fn bwt_decode(mtf: &[u8], main_index: usize) -> Vec<u8> {
+    let mut output: Vec<u8> = Vec::new();
+    unsafe {
+        let mut data= 
+             Bwt::<u8, _>::from_parts(mtf, main_index)
+            .unbwt()
+            .with_owned_temporary_array_buffer32()
+            .multi_threaded(ThreadCount::openmp_default())
+            .run()
+            .unwrap();
+
+        output = data.as_slice().to_vec();
+    }
+
+    output
 }
 
 fn mtf_encode(bwt: &Vec<u8>) -> Vec<u8> {
@@ -99,6 +116,29 @@ fn mtf_encode(bwt: &Vec<u8>) -> Vec<u8> {
     }
 
     output
+}
+
+fn mtf_decode(rle: &Vec<u8>) -> Vec<u8> {
+    let mut symbols = [0u8; 256];
+    for i in 0..=255 {
+        symbols[i] = i as u8;
+    }
+
+    let mut output = Vec::with_capacity(rle.len());
+
+    for &pos in rle {
+        let idx = pos as usize;
+        let byte = symbols[idx];
+        output.push(byte);
+
+        // move to front
+        for i in (1..=idx).rev() {
+            symbols[i] = symbols[i - 1];
+        }
+        symbols[0] = byte;
+    }
+
+    output    
 }
 
 fn rle_encode(mtf: &Vec<u8>) -> Vec<u8> {
@@ -130,6 +170,24 @@ fn rle_encode(mtf: &Vec<u8>) -> Vec<u8> {
     output
 }
 
+fn rle_decode(data: &Vec<u8>) -> Vec<u8> {
+    let mut output = Vec::new();
+    let mut i = 0;
+
+    while i < data.len() {
+        if data[i] == 0 {
+            let run = data[i + 1] as usize;
+            output.extend(std::iter::repeat(0).take(run));
+            i += 2;
+        } else {
+            output.push(data[i]);
+            i += 1;
+        }
+    }
+
+    output
+}
+
 #[derive(Debug, Default)]
 struct Apy {
     file_path: String,
@@ -144,31 +202,99 @@ fn new() -> (Apy, Task<Message>) {
     )
 }
 
-fn decode_file(file_data: Vec<u8>) {
-    let mut heap: VecDeque<_> = file_data.into();
-    let mut map: HashMap<u8, u16> = HashMap::new();
+fn get_prefix(data: &Vec<u8>, index: &mut usize, bit: &mut u8) -> u8 {
+    let mut count: u8 = 0;
+    let mut prefix: u8 = 0;
 
-    let mut cur_data: u16 = 0;
-    let mut not_used: u8 = 0;
-    let mut data_len: u64 = 0;
-    for _i in 0..7 {
-        cur_data = heap.pop_front().unwrap() as u16;
-        cur_data <<= 8;
-        cur_data |= heap.pop_front().unwrap() as u16;
-        map.insert(heap.pop_front().unwrap(), cur_data);
-        not_used = heap.pop_front().unwrap();
+    let mut byte = data[*index];
 
-        for _y in 0..8 {
-            data_len <<= 8;
-            data_len |= heap.pop_front().unwrap() as u64;
+    while count != 3 {
+        prefix = (prefix << 1) | (byte & 1);
+        count += 1;
+
+        *bit += 1;
+        if *bit == 8 {
+            *index += 1;
+            *bit = 0;
+            if count != 3 { byte = data[*index] }
         }
-    } 
+    }
+
+    return prefix;
 }
 
-fn encode_file(api: &mut Apy) {
-    let file_data: Vec<u8> = fs::read(&api.file_path).expect("failed to read file");
+fn decode_file(file_path: String) {
 
-    let (bwt, idx) = bwt_block(&file_data);
+    let file_data: Vec<u8> = fs::read(&file_path).expect("failed to read file");
+
+    let mut heap: VecDeque<_> = file_data.into();
+    let mut map: HashMap<u8, u8> = HashMap::new();
+
+    let mut not_used: Vec<u8> = Vec::new();
+    let mut data_len: Vec<usize> = Vec::new();
+
+
+    //we check if its a compressed file
+    let mut buf = [0u8; 4];
+    for i in 0..4 {
+        buf[i] = heap.pop_front().unwrap();
+    }
+
+    if &buf != b"loli" {
+        println!("Got bamboozled, the file is not even a loli");
+        return
+    }
+
+    //we retrive the bwt encoding index
+    let bwt_index = heap.pop_front();
+
+    //we get the huffman coding map
+    for _i in 0..8 {
+        let encoded = heap.pop_front().unwrap();
+        map.insert(heap.pop_front().unwrap(), encoded);
+    }
+
+    //we get the not used bits count
+    for _i in 0..3 { not_used.push(heap.pop_front().unwrap()) }
+
+    //we get the size of the data chunks
+    for _i in 0..3 { 
+        let mut cur_len: usize = 0;
+        for _y in 0..8 {
+            cur_len = (cur_len << 8) | (heap.pop_front().unwrap() as usize);
+        }
+        data_len.push(cur_len as usize);
+    }
+
+    let main_chunk: Vec<u8> = heap.drain(..data_len[0]).collect();
+    let layer_chunk: Vec<u8> = heap.drain(..data_len[1]).collect();
+    let mut leftover_chunk:  VecDeque<_> = heap.drain(..data_len[2]).collect();
+
+    let mut original: Vec<u8> = Vec::new();
+    let mut layer_index: usize = 0;
+    let mut layer_bit: u8 = 0;
+
+    //undoing huffman coding
+    for byte in &main_chunk {
+        for i in (0..8).rev() {
+            let bit = (byte >> i) & 1;
+            if bit == 1 { original.push(map[&get_prefix(&layer_chunk, &mut layer_index, &mut layer_bit)]) } 
+            else { original.push(leftover_chunk.pop_front().unwrap()) };
+        }
+    }
+
+
+
+
+    let output_path: String = format!("{}.gif", file_path.split('.').next().unwrap());
+    fs::write(output_path, original).expect("cant write decoded data");
+
+}
+
+fn encode_file(file_path: String) {
+    let file_data: Vec<u8> = fs::read(&file_path).expect("failed to read file");
+
+    let (bwt, idx) = bwt_encode(&file_data);
     println!("did bwt");
     let mtf = mtf_encode(&bwt);
     println!("did mtf");
@@ -247,37 +373,38 @@ fn encode_file(api: &mut Apy) {
 
     let mut output: Vec<u8> = Vec::new();
     output.extend_from_slice("LOLI".as_bytes());
+
+    //original index for bwt decoding
+    output.extend_from_slice(&(idx as u64).to_le_bytes());
      
     for item in &layer.encoded {
         output.push(*item.0);
         output.push(*item.1);
     }
-    let not_used: u8 = 8 - layer.chunk.bit_count;
-    if layer.chunk.bit_count > 0 {
-        layer.chunk.block.push(layer.chunk.buffer << not_used);
-    }
-    output.push(not_used % 8);
-    output.extend_from_slice(&(layer.chunk.block.len() as u64).to_le_bytes());
 
+    //pushes not used bits for the three chunk
     let mut not_used: u8 = 8 - main_chunk.bit_count;
-    if main_chunk.bit_count > 0 {
-        main_chunk.block.push(main_chunk.buffer << not_used);
-    }
+    if main_chunk.bit_count > 0 { main_chunk.block.push(main_chunk.buffer << not_used) }
+    output.push(not_used % 8);
+
+    not_used = 8 - layer.chunk.bit_count;
+    if layer.chunk.bit_count > 0 { layer.chunk.block.push(layer.chunk.buffer << not_used) }
     output.push(not_used % 8);
 
     not_used = 8 - left_over.bit_count;
-    if left_over.bit_count > 0 {
-        left_over.block.push(left_over.buffer << not_used);
-    }
+    if left_over.bit_count > 0 { left_over.block.push(left_over.buffer << not_used) }
     output.push(not_used % 8);
 
-    output.extend_from_slice(&main_chunk.block);
+    //append chunksize and chunks
+    output.extend_from_slice(&(main_chunk.block.len() as u64).to_le_bytes());
+    output.extend_from_slice(&(layer.chunk.block.len() as u64).to_le_bytes());
+    output.extend_from_slice(&(left_over.block.len() as u64).to_le_bytes());
 
+    output.extend_from_slice(&main_chunk.block);
     output.extend_from_slice(&layer.chunk.block);
-    
     output.extend_from_slice(&left_over.block);
 
-    let output_path: String = format!("{}.loli", api.file_path.split('.').next().unwrap());
+    let output_path: String = format!("{}.loli", file_path.split('.').next().unwrap());
     fs::write(output_path, output).expect("cant write encoded data");
 }
 
@@ -313,7 +440,11 @@ fn update(api: &mut Apy, message: Message) {
     match message {
         Message::FileDropped(path) => {
             api.file_path = path;
-            encode_file(api);
+            if api.file_path.split('.').last().unwrap() == "loli" {
+                decode_file(api.file_path.clone());
+            } else {
+                encode_file(api.file_path.clone());
+            }
         }
         Message::None => {}
     }
@@ -331,7 +462,6 @@ fn view(api: &Apy) -> Element<'_, Message> {
     ];
     column(children).into()
 }
-
 fn subscription(_api: &Apy) -> Subscription<Message> {
     event::listen().map(|event| match event {
         Event::Window(window::Event::FileDropped(path)) => {
